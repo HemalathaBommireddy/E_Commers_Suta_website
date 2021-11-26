62825f1549bce124a71017aede761be4bc430ee7
HemalathaBommireddy <hbommireddy@innominds.com>
2021-11-26 16:13:28 +0530

    Created Component of LoginPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SignUpComponent } from './sign-up.component';
-import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    SignUpComponent,
-    NavigationbarComponent
+    LoginPageComponent,
+    
   ],
   imports: [
     BrowserModule,
