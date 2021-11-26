2bbe952eb289b434eb23084d63002d196de9834f
HemalathaBommireddy <hbommireddy@innominds.com>
2021-11-26 11:51:05 +0530

    Changes in signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from './sign-up.component';
 import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 
 @NgModule({
