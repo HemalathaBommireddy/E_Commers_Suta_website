a272b9750172656d4c7869e49665426cd0cb483a
HemalathaBommireddy <hbommireddy@innominds.com>
2021-11-27 18:46:12 +0530

    forgotpassword page successfully created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupComponent } from './signup/signup.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   {path:'',component:LoginPageComponent},
-  {path:'signup',component:SignupComponent
+  {path:'signup',component:SignupComponent},
+  {path:'forgot-password',component:ForgotPasswordComponent},
+  {path:'landing-page',component:LandingPageComponent
 }
 ];
 
