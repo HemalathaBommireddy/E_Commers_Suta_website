dfd9f27b1213e5d7aed8ba43c9a4102a10e05896
HemalathaBommireddy <hbommireddy@innominds.com>
2021-12-02 11:00:31 +0530

    FrontEnd Completed

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  public data={
+    
+    UserName: '',
+    Email : '',
+    Password : '',
+    ConformPassword : ''
+  }
 
+  valid = {
+    UserName: true,
+    Email: true,
+    Password: true,
+    ConformPassword:true,
+    PhoneNumber:true
+  };
   constructor() { }
 
   ngOnInit(): void {
   }
+  validate(type: string): void {
+    const usernamePattern = /^[a-zA-Z]+$/;
+    const emailPattern = /\S+@\S+\.\S+/;
 
-}
+    if (type === 'UserName') {
+      if (this.data.UserName.length < 5) {
+        this.valid.UserName = false;
+      } else {
+        this.valid.UserName = usernamePattern.test(this.data.UserName);
+      }
+    } else if (type === 'Email') {
+      this.valid.Email = emailPattern.test(this.data.Email);
+    } else if (type === ('conformPassword' || 'Password')) {
+      if (this.data.Password === this.data.ConformPassword) {
+        this.valid.Password = true;
+      } else {
+        this.valid.Password = false;
+      }
+    }
+  }
+
+  onkey(event: any, type: string) {
+    if (type === 'UserName') {
+      this.data.UserName = event.target.value;
+    } else if (type === 'Email') {
+      this.data.Email = event.target.value;
+    } else if (type === 'Password') {
+      this.data.Password = event.target.value;
+    } else if (type === 'ConformPassword') {
+      this.data.ConformPassword = event.target.value;
+    }
+    this.validate(type);
+  }
+
+
+   
+  }
+
+  
+
+  
+
